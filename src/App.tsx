28ff9ab8f25fab54ea7d4a8cc3299d09541f94ed
wayne <weijiewang001@gmail.com>
2023-06-20 16:34:10 +1000

    no mes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+const products = [
+  { name: 'product1', price: 100.00 },
+  { name: 'product2', price: 200.00 },
+]
 
 function App() {
+  const [products, setProducts] = useState([
+    { name: 'product1', price: 100.00 },
+    { name: 'product2', price: 200.00 },
+  ]);
+
+  useEffect(()=> {
+    fetch('http://localhost:5000/api/products')
+     .then(response => response.json())
+     .then(data => setProducts(data))
+  }, [])
+
+
+  function addProduct() {
+    setProducts(prevState => [...prevState, { name: 'product' + (prevState.length + 1), price: ((prevState.length + 1) * 100.00) }])
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{ color: "blue" }}>Re-Store</h1>
+      <ul>
+        {products.map(item => (
+          <li key={item.name}>{item.name} - {item.price}</li>
+        ))}
+      </ul>
+      <button onClick={addProduct}>Add product</button>
     </div>
   );
 }
