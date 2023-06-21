b71a570d51b49d80d7afe1da943d01af16d79b6c
wayne <weijiewang001@gmail.com>
2023-06-22 01:13:09 +1000

    add <CardActionArea> to splite background color and card color

--- src/features/catalog/ProductCard.tsx
+++ src/features/catalog/ProductCard.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Typography } from "@mui/material"
+import { Avatar, Button, Card, CardActionArea, CardActions, CardContent, CardHeader, CardMedia, Typography } from "@mui/material"
 import { Product } from "../../app/models/product"
 
 interface Props {
@@ -7,35 +7,37 @@ interface Props {
 
 export const ProductCard = ({ product }: Props) => {
   return (
-    <div>
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: 'primary.main' }}>
-            {product.name.charAt(0).toUpperCase()}
-          </Avatar>
-        }
-        title={product.name}
-        titleTypographyProps={{
-          sx: { fontWeight: 'bold', color: 'text.primary' }
-        }}
-      />
-      <CardMedia
-        sx={{ height: 140, backgroundSize: 'contain', bgcolor: 'background.delt' }}
-        image={product.pictureUrl}
-        title={product.name}
-      />
-      <CardContent>
-        <Typography gutterBottom color="secondary" variant="h5">
-          A${(product.price / 100).toFixed(2)}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {product.brand} / {product.type}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Add to cart</Button>
-        <Button size="small">View</Button>
-      </CardActions>
-    </div>
+    <Card>
+      <CardActionArea>
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: 'primary.main' }}>
+              {product.name.charAt(0).toUpperCase()}
+            </Avatar>
+          }
+          title={product.name}
+          titleTypographyProps={{
+            sx: { fontWeight: 'bold', color: 'text.primary' }
+          }}
+        />
+        <CardMedia
+          sx={{ height: 140, backgroundSize: 'contain', bgcolor: 'background.delt' }}
+          image={product.pictureUrl}
+          title={product.name}
+        />
+        <CardContent>
+          <Typography gutterBottom color="secondary" variant="h5">
+            A${(product.price / 100).toFixed(2)}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {product.brand} / {product.type}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Add to cart</Button>
+          <Button size="small">View</Button>
+        </CardActions>
+      </CardActionArea>
+    </Card>
   )
 }
